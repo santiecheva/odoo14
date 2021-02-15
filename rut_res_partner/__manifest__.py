9ecc1c38aae93ca06abdf893a20bd69a81676139
Santiago Echeverri <santiago.echeverri@pragma.com.co>
2021-02-15 00:54:46 -0500

    contacto rut

--- rut_res_partner/__manifest__.py
+++ rut_res_partner/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 {
-    'name': "Crm Cumplimiento",
+    'name': "Formulario Contacto Reglas Colombianas",
     'version': '14.0.2.0.1',
     'summary': """Fórmulario para los negocios de cumplimiento""",
     'description': """Este módulo tiene los campos necesarios para guardar un contacto""",
