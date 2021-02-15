e5c136776bfd5f6c55c65ff934ad5cc7a3d4b1e4
Santiago Echeverri <santiago.echeverri@pragma.com.co>
2021-02-15 00:36:39 -0500

    contacto rut

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-cd /home/ubuntu/miodoo
+cd /home/ubuntu
 git init
-git pull origin master
+git pull origin main
 sudo cp -r /home/ubuntu/odoo14/rut_res_partner /odoo/custom/addons/
 sudo service odoo-server restart
 
