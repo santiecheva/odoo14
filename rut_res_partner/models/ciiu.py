5557e49c5e516373d336f96100d824f7914455e4
Santiago Echeverri <santiago.echeverri@pragma.com.co>
2021-02-28 21:55:54 -0500

    tipo de persona

--- rut_res_partner/models/ciiu.py
+++ rut_res_partner/models/ciiu.py
@@ -6,6 +6,4 @@ from odoo import models, fields, api
 class CIIU(models.Model):
     _name = "ciiu" 
 
-    name = fields.Char(
-        string="Code and Description",
-    )
+    name = fields.Char(string="Code and Description")
