b51b8dce4beae8a45e815f95731a4410480c16cd
Santiago Echeverri <santiago.echeverri@pragma.com.co>
2021-02-28 21:28:58 -0500

    tipo de persona

--- rut_res_partner/models/ciiu.py
+++ rut_res_partner/models/ciiu.py
@@ -9,13 +9,3 @@ class CIIU(models.Model):
     name = fields.Char(
         string="Code and Description",
     )
-    code = fields.Char(string = 'Code', required=True)
-    description = fields.Char(string ='Description', required=True)
-    
-    @api.onchange('code','description')
-    def _compute_concat_name(self):
-        for record in self:
-            if record.code is False or record.description is False:
-                record.name = ''
-            else:
-                record.name = str(record.code) + ' - ' + str(record.description)
