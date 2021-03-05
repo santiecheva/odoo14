035c396c844184103c47501cfe4a49418ba0d1d2
Santiago Echeverri <santiago.echeverri@pragma.com.co>
2021-03-05 08:54:14 -0500

    ciiu

--- rut_res_partner/models/ciiu.py
+++ rut_res_partner/models/ciiu.py
@@ -7,3 +7,5 @@ class CIIU(models.Model):
     _name = "ciiu" 
 
     name = fields.Char(string="Code and Description")
+    code = fields.Char(string="Code")
+    descripion = fields.Char(string="Description")
