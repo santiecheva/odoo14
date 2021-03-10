bc70c3ca965a3e7a03de51bca287eef2e99126c9
Santiago Echeverri <santiago.echeverri@pragma.com.co>
2021-03-10 06:58:55 -0500

    gantt for project

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,6 @@
 cd /home/ubuntu/odoo14
 git init
 git pull origin main
-sudo cp -r /home/ubuntu/odoo14/rut_res_partner /odoo/custom/addons/
+sudo cp -r /home/ubuntu/odoo14/web_gantt_project_task_app /odoo/custom/addons/
 sudo service odoo-server restart
 
