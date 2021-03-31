d39aec577a54747e60d43e7c5975ad801496558c
Santiago Echeverri <santiago.echeverri@pragma.com.co>
2021-03-31 15:34:21 -0500

    odoo

--- crm_cumplimiento/__manifest__.py
+++ crm_cumplimiento/__manifest__.py
@@ -5,7 +5,7 @@
     'version': '14.0.2.0.1',
     'summary': """Fórmulario para los negocios de cumplimiento""",
     'description': """Este módulo tiene los campos necesarios para tramitar una póliza de cumplimiento
-                    por ejemplo, valor de contrato, asegurado, beneficiario""",
+                    por ejemplo, valor de contrato, asegurado, beneficiario """,
     'author': "Santiago Echeverri Valencia",
     'company': "Rave Agencia de Seguros",
     'maintainer': 'Santiago Echeverri Valencia',
