0473fc8287db57b9e564875f1f05863d5dfe7ff6
Santiago Echeverri <santiago.echeverri@pragma.com.co>
2021-03-31 16:21:20 -0500

    nombres

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 cd /home/ubuntu/odoo14
-git init
 git pull origin main
 sudo cp -r /home/ubuntu/odoo14/crm_cumplimiento /odoo/custom/addons/
 sudo cp -r /home/ubuntu/odoo14/rut_res_partner /odoo/custom/addons/
