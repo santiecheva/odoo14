a62015cef1cb3beba6bd8df71bdfbed7a9797161
Santiago Echeverri <santiago.echeverri@pragma.com.co>
2021-04-07 00:40:00 -0500

    concatenar nombres

--- rut_res_partner/models/res_partner.py
+++ rut_res_partner/models/res_partner.py
@@ -38,6 +38,11 @@ class ResPartner(models.Model):
     secondName = fields.Char(string ="Second Name")
     lastName = fields.Char(string ="Last Name")
     secondLastName = fields.Char(string ="Second Last Name")
+    # Company Name (legal name)
+    companyName = fields.Char(string="Company Name")
+
+    # Brand Name (e.j. Claro Móvil = Brand, COMCEL SA = legal name)
+    companyBrandName = fields.Char(string ="Brand")
 
     x_pn_retri = fields.Selection(
         [
@@ -57,3 +62,74 @@ class ResPartner(models.Model):
         'UNIQUE(l10n_latam_identification_type_id,vat)',
         "¡El número de identificación debe ser único!"),
     ]
+
+    @api.onchange('firstName', 'secondName', 'lastName', 'secondLastName', 'companyName', 'pos_name')
+    def _concat_name(self):
+        """
+            This function concatenates the four name fields in order to be able to
+            search for the entire name. On the other hand the original name field
+            should not be editable anymore as the new name fields should fill it up
+            automatically.
+            @return: void
+        """
+        # Avoiding that "False" will be written into the name field
+        if not self.firstName:
+            self.firstName = ''
+
+        if not self.secondName:
+            self.secondName = ''
+
+        if not self.lastName:
+            self.lastName = ''
+
+        if not self.secondLastName:
+            self.secondLastName = ''
+
+        # Collecting all names in a field that will be concatenated
+        nameList = [
+            self.firstName.encode(encoding='utf-8').strip(),
+            self.secondName.encode(encoding='utf-8').strip(),
+            self.lastName.encode(encoding='utf-8').strip(),
+            self.secondLastName.encode(encoding='utf-8').strip()
+        ]
+
+        formatedList = []
+        if self.companyName is False:
+            if self.type == 'delivery':
+                self.name = self.pos_name
+                self.firstName = False
+                self.secondName = False
+                self.lastName = False
+                self.secondLastName = False
+            else:
+                for item in nameList:
+                    if item is not b'':
+                        formatedList.append(item.decode('UTF-8'))
+                    self.name = ' '.join(formatedList)
+        else:
+            # Some Companies are know for their Brand, which could conflict from the users point of view while
+            # searching the company (e.j. o2 = brand, Telefonica = Company)
+            if self.companyBrandName is not False:
+                delimiter = ', '
+                company_list = (self.companyBrandName, self.companyName)
+                self.name = delimiter.join(company_list)
+            else:
+                self.name = self.companyName
+
+    @api.onchange('name')
+    def on_change_name(self):
+        """
+        The name field gets concatenated by the four name fields.
+        If a user enters a value anyway, the value will be deleted except first
+        name has no value. Reason: In certain forms of odoo it is still
+        possible to add value to the original name field. Therefore we have to
+        ensure that this field can receive values unless we offer the four name
+        fields.
+        @return: void
+        """
+        if self.x_name1 is not False:
+            if len(self.x_name1) > 0:
+                self._concat_name()
+        if self.companyName is not False:
+            if len(self.companyName) > 0:
+                self._concat_name()
